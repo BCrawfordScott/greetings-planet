82149255c1addd1c9a0753be7ebc2e730a44f983
BCrawfordScott <briancrawfordscott@gmail.com>
2021-02-22 18:16:38 -0600

    initial commit

---
+++ frontend/src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+export default (props) => {
+    return (
+        <div className="home">
+            <marquee>
+                <h1>Hello World!</h1>
+            </marquee>
+        </div>
+    )
+}
